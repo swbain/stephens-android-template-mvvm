b176f2bb932753206036b1580731ce0c6ed223c7
Stephen Bain <swbain@gmail.com>
2020-05-17 21:32:43 -0500

    Added example data fetch with coroutine

--- app/src/main/java/com/stephenbain/androidtemplate/home/HomeFragment.kt
+++ app/src/main/java/com/stephenbain/androidtemplate/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.stephenbain.androidtemplate.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.lifecycle.observe
 import com.stephenbain.androidtemplate.R
 import com.stephenbain.androidtemplate.common.BaseFragment
@@ -16,8 +17,22 @@ class HomeFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.state.observe(viewLifecycleOwner) { state ->
-            text.text = state.text
-        }
+        viewModel.state.observe(viewLifecycleOwner, ::handleState)
+    }
+
+    private fun handleState(state: HomeState) = when (state) {
+        HomeState.Loading -> showLoading()
+        is HomeState.Success -> showHomeText(state.text)
+    }
+
+    private fun showLoading() {
+        text.isVisible = false
+        loading.isVisible = true
+    }
+
+    private fun showHomeText(homeText: String) {
+        text.text = homeText
+        text.isVisible = true
+        loading.isVisible = false
     }
 }
--- app/src/main/java/com/stephenbain/androidtemplate/home/HomeViewModel.kt
+++ app/src/main/java/com/stephenbain/androidtemplate/home/HomeViewModel.kt
@@ -3,17 +3,42 @@ package com.stephenbain.androidtemplate.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class HomeViewModel @Inject constructor() : ViewModel() {
+/**
+ * Example ViewModel fetching data using a coroutine. Delete and replace as needed.
+ */
+class HomeViewModel @Inject constructor(private val getHomeText: GetHomeText) : ViewModel() {
     private val _state = MutableLiveData<HomeState>()
 
     val state: LiveData<HomeState>
         get() = _state
 
     init {
-        _state.value = HomeState("haha this is ur app lol")
+        _state.value = HomeState.Loading
+        viewModelScope.launch {
+            val text = withContext(Dispatchers.IO) { getHomeText() }
+            _state.value = HomeState.Success(text)
+        }
     }
 }
 
-data class HomeState(val text: String)
+sealed class HomeState {
+    object Loading : HomeState()
+    data class Success(val text: String) : HomeState()
+}
+
+/**
+ * Example of using a coroutine to fetch data. delete and replace as necessary
+ */
+class GetHomeText @Inject constructor() {
+    suspend operator fun invoke(): String {
+        delay(2000)
+        return "haha this is ur app lol"
+    }
+}
